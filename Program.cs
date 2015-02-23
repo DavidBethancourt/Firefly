880408fe3d2593eaac669027b0655c9aef5be9d4
David Bethancourt <davidbethancourt@gmail.com>
2015-02-23 12:58:18 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Firefly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
